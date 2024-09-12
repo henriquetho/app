7781b7be4e410a524dac99fcb3c221fef09b7056
henriquetho <henrique.ribeirotho@gmail.com>
2024-09-11 21:23:55 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-// hello world
-console.log("hello world!");
+let meta = {
+    value: 'Ler um livro por dia.',
+    ckecked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: 'Caminhar 20 minutos todos os dias.',
+        checked: false,
+    }
+]
+
+console.log(metas[1].value)
